a5d05f6c0ba5d88018cb4538dc8c381a79da2b4f
chavdar <chavdar.rakov@gmail.com>
2020-11-26 10:13:50 +0200

    Update main.tf

--- test/main.tf
+++ test/main.tf
@@ -4,6 +4,22 @@ resource "random_id" "server" {
   byte_length = 9
 }
 
+resource "null_resource" "server" {
+  provisioner "local-exec" {
+    command = "echo ${random_id.server.hex}"
+  }
+}
+
+resource "random_pet" "server1" {
+  keepers = {}
+}
+
+resource "null_resource" "server1" {
+  provisioner "local-exec" {
+    command = "echo ${random_pet.server1.id}"
+  }
+}
+
 resource "null_resource" "release" {
   provisioner "local-exec" {
     command = "cat /etc/*release"
